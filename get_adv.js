5787ccabcd9858bebf7a607bff25598e9a0f93d4
island-web <118316108+island-web@users.noreply.github.com>
2022-12-22 13:43:25 +0200

    Update get_adv.js

--- get_adv.js
+++ get_adv.js
@@ -16,7 +16,7 @@ let connection_adv = mysql.createConnection({
     password: 'nBAw8R03mKti'
 });
 
-
+console.log('CHECK ADV');
 function getAdv() {
     connection.query(`SELECT id_program FROM stations_program WHERE id_station = ${db.get("id")}`,
         function (err, results) {
@@ -30,12 +30,19 @@ function getAdv() {
                 results.forEach(ad => {
                     connection_adv.query(`SELECT * FROM adv_program WHERE id_program = ${ad.id_program}`,
                         function (err, results_adv) {
-                            results_adv.forEach(r_ad => {
-                                if (r_ad.date_stop > new Date(today())) { temp_ad.push(r_ad); }
-                            });
-                            if (process.send) {
-                                process.send('NEW_ADV', results);
-                                db.set("adv", temp_adv);
+                            if(err){
+                                if (process.send) {
+                                    process.send('ERROR CONNECTION TO [DATA_ADV], SERVER DOWN');
+                                }    
+                            }
+                            else{
+                                results_adv.forEach(r_ad => {
+                                    if (r_ad.date_stop > new Date(today())) { temp_ad.push(r_ad); }
+                                });
+                                if (process.send) {
+                                    process.send('NEW_ADV', results);
+                                    db.set("adv", temp_adv);
+                                }    
                             }
                         });
                 });
