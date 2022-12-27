9773921dddcc6d7654a6609e287353b6f427f4d5
island-web <118316108+island-web@users.noreply.github.com>
2022-12-27 22:09:53 +0200

    Update start_module_init.js

--- start_module_init.js
+++ start_module_init.js
@@ -9,6 +9,8 @@ shell.rm('-rf', '/home/pi/Desktop/TMM_RASPBERRY/tmm.zip');
 shell.exec('npm node monit_start.js');
 
 shell.exec('sudo npx pm2 start ecosystem.config.js');
+shell.exec('sudo npx pm2 startup');
+shell.exec('sudo npx pm2 save');
 
 
 
